e3ced0a53176aa5986648b77586240d8274c676d
Rohan Kumar <rk399504@gmail.com>
2024-05-08 15:00:32 +0530

    Update file-handling

--- file-handling/main.go
+++ file-handling/main.go
@@ -2,26 +2,42 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 )
 
 func main() {
-	file, err := os.Create("example.txt")
+
+	// --------- write file --------
+
+	// file, err := os.Create("example.txt")
+	// if err != nil {
+	// 	fmt.Println("Error while creating file")
+	// 	return
+	// }
+	// defer file.Close()
+	// content := "Hello Rohan!"
+	// // file.Write([]byte(content))  //Method 1 (self)
+
+	// // Method 2
+	// _, errors := io.WriteString(file, content+"\n")
+	// if errors != nil {
+	// 	fmt.Println("Error while writing file: ", errors)
+	// 	return
+	// }
+
+	// fmt.Println("Successfully Created file!!")
+
+
+
+	// -------- READ file--------
+	// there are basically two methods to read the file : a. load complete file in memory and  b. load in chuncks using buffer.
+
+	// METHOD 1: load complete file
+	content, err := os.ReadFile("example.txt")
 	if err != nil {
-		fmt.Println("Error while creating file")
+		fmt.Println("Erro while reading the file.", err)
 		return
 	}
-	defer file.Close()
-	content := "Hello Rohan!"
-	// file.Write([]byte(content))  //Method 1
+	fmt.Println(string(content))
 
-	// Method 2
-	_, errors := io.WriteString(file, content+"\n")
-	if errors != nil {
-		fmt.Println("Error while writing file: ", errors)
-		return
-	}
-
-	fmt.Println("Successfully Created file!!")
 }
