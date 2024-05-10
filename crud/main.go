e3e302c9f053ff043ed8e48baa8b0a4ebe01a0c8
Rohan Kumar <rk399504@gmail.com>
2024-05-10 11:49:32 +0530

    Added POST request

--- crud/main.go
+++ crud/main.go
@@ -3,17 +3,19 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 type Todo struct {
 	UserID int    `json:"userId"`
 	Id     int    `json:"id"`
 	Title  string `json:"title"`
+	Completed bool `json:c"om"`
 }
 
-func main() {
-	fmt.Println("CRUD loading...")
+func performGetRequest() {
 	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
 	if err != nil {
 		fmt.Println("Something went wring", err)
@@ -40,5 +42,53 @@ func main() {
 		return
 	}
 	fmt.Println("Todo", todo)
+}
+
+func performPostRequest() {
+	todo := Todo{
+		UserID :  23,
+		Title:    "Rohan Kumar",
+		Completed: true,
+	}
+
+	// Convert the todo struct to JSON
+	// res, err = http.Post("https://jsonplaceholder.typicode.com/todos/")
+
+	jsonData, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("Erro Marshaling ", err)
+		return
+	}
+	// Convert Json data to string
+	jsonString := string(jsonData)
+
+	//convert string data to reader
+	jsonReader := strings.NewReader(jsonString)
+
+	myUrl := "https://jsonplaceholder.typicode.com/todos"
+
+	// POST REQUEST
+	res, err := http.Post(myUrl, "application/json", jsonReader)
+	if err != nil {
+		fmt.Println("Error in Post request", err)
+		return
+	}
+	defer res.Body.Close()
+
+	// convert response in readable format (As POST request give response)
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error in POST response", err)
+		return
+	}
+	fmt.Println("Response is: ", string(data))
+	fmt.Println("Response Status: ",res.Status)
+
+}
+
+func main() {
+	fmt.Println("CRUD loading...")
+	// performGetRequest()
+	performPostRequest()
 
 }
